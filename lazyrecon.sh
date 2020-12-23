611f96b5a49d788729c939648e0a0e9ffb5dff53
storenth <storenth@yandex.ru>
2020-12-23 13:19:16 +0700

    Update 2.5.1: alt and wildcard arguments, massdns/shuffledns, masscan/nmap NSE optimization

--- lazyrecon.sh
+++ lazyrecon.sh
@@ -4,96 +4,135 @@
 
 # Config
 altdnsWordlist=./lazyWordLists/altdns_wordlist_uniq.txt
+madSubdomainWordlist=./lazyWordLists/subdomains_wordlist.txt
 dirsearchWordlist=./lazyWordLists/curated.txt
-resolvers=./resolvers/mini_resolvers.txt
+miniResolvers=./resolvers/mini_resolvers.txt
+madResolvers=./resolvers/resolvers.txt
 dirsearchThreads=200
 
 # optional positional arguments
-brute=
-mad=
+brute= # enable directory bruteforce
+mad= # if you sad about subdomains count, call it
+alt= # permutate and alterate subdomains
+wildcard= # fight against multi-level wildcard DNS to avoid false-positive results while subdomain resolves
 
 # definitions
 enumeratesubdomains(){
-  echo "[phase 1] Enumerating all known domains using:"
+  echo "Enumerating all known domains using:"
 
   # Passive subdomain enumeration
   echo "subfinder..."
   subfinder -d $1 -o ./$1/$foldername/subfinder-list.txt
   echo "assetfinder..."
   assetfinder --subs-only $1 > ./$1/$foldername/assetfinder-list.txt
+  # echo "amass..."
+  # amass enum --passive -log ./$1/$foldername/amass_errors.log -d $1 -o ./$1/$foldername/amass-list.txt
 
-  # Active subdomain enumeration (--mad option only)
-  if [ "$mad" = "1" ]; then
-    echo "amass bruteforcing..."
-    amass enum -brute -min-for-recursive 2 -rf $resolvers -log ./$1/$foldername/amass_errors.log -d $1 -o ./$1/$foldername/amass-list.txt
-    echo "shuffledns bruteforcing..."
-    shuffledns -d $1 -retries 1 -r $resolvers -w $altdnsWordlist -o ./$1/$foldername/shuffledns-list.txt
-  fi
   # sort enumerated subdomains
-  sort -u ./$1/$foldername/subfinder-list.txt ./$1/$foldername/amass-list.txt ./$1/$foldername/assetfinder-list.txt ./$1/$foldername/shuffledns-list.txt > ./$1/$foldername/enumerated-subdomains.txt
+  sort -u ./$1/$foldername/subfinder-list.txt ./$1/$foldername/amass-list.txt ./$1/$foldername/assetfinder-list.txt -o ./$1/$foldername/enumerated-subdomains.txt
 }
 
 checkwaybackurls(){
-  echo "gau..."
-  # gau -subs mean include subdomains
-  cat ./$1/$foldername/enumerated-subdomains.txt | gau -subs -o ./$1/$foldername/gau_output.txt
-  echo "waybackurls..."
-  cat ./$1/$foldername/enumerated-subdomains.txt | waybackurls > ./$1/$foldername/waybackurls_output.txt
-
-  # wayback_output.txt needs for checkparams
-  sort -u ./$1/$foldername/gau_output.txt ./$1/$foldername/waybackurls_output.txt > ./$1/$foldername/wayback_output.txt
-  cat ./$1/$foldername/wayback_output.txt | unfurl --unique domains > ./$1/$foldername/wayback-subdomains-list.txt
-
-  # prepare target specific subdomains wordlist to gain more subdomains using --mad mode
   if [ "$mad" = "1" ]; then
+    echo "gau..."
+    # gau -subs mean include subdomains
+    cat ./$1/$foldername/enumerated-subdomains.txt | gau -subs -o ./$1/$foldername/gau_output.txt
+    echo "waybackurls..."
+    cat ./$1/$foldername/enumerated-subdomains.txt | waybackurls > ./$1/$foldername/waybackurls_output.txt
+
+    # wayback_output.txt needs for checkparams
+    sort -u ./$1/$foldername/gau_output.txt ./$1/$foldername/waybackurls_output.txt -o ./$1/$foldername/wayback_output.txt
+    cat ./$1/$foldername/wayback_output.txt | unfurl --unique domains > ./$1/$foldername/wayback-subdomains-list.txt
+  fi
+  if [ "$alt" = "1" -a "$mad" = "1" ]; then
+    # prepare target specific subdomains wordlist to gain more subdomains using --mad mode
     cat ./$1/$foldername/wayback_output.txt | unfurl format %S | sort | uniq > ./$1/$foldername/wayback-subdomains-wordlist.txt
-    sort -u $altdnsWordlist ./$1/$foldername/wayback-subdomains-wordlist.txt -o $altdnsWordlist
+    sort -u $altdnsWordlist ./$1/$foldername/wayback-subdomains-wordlist.txt -o $customSubdomainsWordList
   fi
 }
 
 sortsubdomains(){
-  sort -u ./$1/$foldername/enumerated-subdomains.txt ./$1/$foldername/wayback-subdomains-list.txt > ./$1/$foldername/1-real-subdomains.txt
+  sort -u ./$1/$foldername/enumerated-subdomains.txt ./$1/$foldername/wayback-subdomains-list.txt -o ./$1/$foldername/1-real-subdomains.txt
+  cp ./$1/$foldername/1-real-subdomains.txt ./$1/$foldername/2-all-subdomains.txt
 }
 
 permutatesubdomains(){
-  echo "altdns..."
-  altdns -i ./$1/$foldername/1-real-subdomains.txt -o ./$1/$foldername/altdns_output.txt -w $altdnsWordlist
+  if [ "$alt" = "1" ]; then
+    echo "altdns..."
+    altdns -i ./$1/$foldername/1-real-subdomains.txt -o ./$1/$foldername/altdns_output.txt -w $customSubdomainsWordList
 
-  sort -u ./$1/$foldername/1-real-subdomains.txt ./$1/$foldername/altdns_output.txt > ./$1/$foldername/2-all-subdomains.txt
+    sort -u ./$1/$foldername/1-real-subdomains.txt ./$1/$foldername/altdns_output.txt -o ./$1/$foldername/2-all-subdomains.txt
+  fi
 }
 
+# check live subdomains
+# wildcard check like: `dig @188.93.60.15 A,CNAME {test123,0000}.$domain +short`
+# shuffledns uses for wildcard because massdn can't
 dnsprobing(){
   # check file wirteup successfully from previous step
-  while [ ! -s ./$1/$foldername/2-all-subdomains.txt ]; do
-    echo "[dnsprobing] 2-all-subdomains.txt empty, sleep 1"
-    sleep 1
-  done
-  echo "dnsprobe..."
-  dnsprobe -l ./$1/$foldername/2-all-subdomains.txt -r A -s $resolvers -o ./$1/$foldername/dnsprobe_output.txt
+  # while [ ! -s ./$1/$foldername/2-all-subdomains.txt ]; do
+  #   echo "[dnsprobing] 2-all-subdomains.txt empty, sleep 1"
+  #   sleep 1
+  # done
+  if [ "$wildcard" = "1" ]; then
+    echo "[shuffledns] massdns probing with wildcard sieving..."
+    shuffledns -d $1 -list ./$1/$foldername/2-all-subdomains.txt -retries 1 -r $madResolvers -o ./$1/$foldername/shuffledns-list.txt
+    # additional resolving because shuffledns missing IP on output
+    echo "[dnsx] dnsprobing..."
+    # echo "[dnsx] wildcard filtering:"
+    # dnsx -l ./$1/$foldername/shuffledns-list.txt -wd $1 -o ./$1/$foldername/dnsprobe_live.txt
+    echo "[dnsx] getting hostnames and its A records:"
+    dnsx -a -resp -l ./$1/$foldername/shuffledns-list.txt -o ./$1/$foldername/dnsprobe_output_tmp.txt
+    # clear file from [ and ] symbols
+    tr -d '\[\]' < ./$1/$foldername/dnsprobe_output_tmp.txt > ./$1/$foldername/dnsprobe_output.txt
+    # split resolved hosts ans its IP (for masscan)
+    cut -f1 -d ' ' ./$1/$foldername/dnsprobe_output.txt | sort | uniq > ./$1/$foldername/dnsprobe_subdomains.txt
+    cut -f2 -d ' ' ./$1/$foldername/dnsprobe_output.txt | sort | uniq > ./$1/$foldername/dnsprobe_ip.txt
+  else
+    echo "[massdns] dnsprobing..."
+    # pure massdns:
+    massdns -r $madResolvers -o S -w ./$1/$foldername/massdns_output.txt ./$1/$foldername/2-all-subdomains.txt
+    cut -f1 -d ' ' ./$1/$foldername/massdns_output.txt | sed 's/.$//' | sort | uniq > ./$1/$foldername/dnsprobe_subdomains.txt
+    cut -f3 -d ' ' ./$1/$foldername/massdns_output.txt | sed 's/.$//' | sort | uniq > ./$1/$foldername/dnsprobe_ip.txt
+  fi
+}
 
-  # split resolved hosts ans its IP (for masscan)
-  cut -f1 -d ' ' ./$1/$foldername/dnsprobe_output.txt > ./$1/$foldername/dnsprobe_subdomains.txt
-  cut -f2 -d ' ' ./$1/$foldername/dnsprobe_output.txt | sort -u > ./$1/$foldername/dnsprobe_ip.txt
+# nmap(){
+#   echo "[phase 7] Test for unexpected open ports..."
+#   nmap -sS -PN -T4 --script='http-title' -oG nmap_output_og.txt
+# }
+masscantest(){
+  # max-rate for accuracy
+  masscan -p1-65535 -iL ./$1/$foldername/dnsprobe_ip.txt --rate 1000 --open-only -oL ./$1/$foldername/masscan_output.txt
+  sed '1d;$d' ./$1/$foldername/masscan_output.txt > ./$1/$foldername/masscan_output_tmp.txt
+}
+
+# scan for specifiec PORTS (21,22,...)
+# -Pn: Treat all hosts as online -- skip host discovery
+# -sV: Probe open ports to determine service/version info
+  #  old approach
+  # nmap --script "discovery,ftp*,ssh*,http-vuln*,mysql-vuln*,imap-*,pop3-*" -iL ./$1/$foldername/nmap_input.txt
+nmap_nse(){
+  # https://gist.github.com/storenth/b419dc17d2168257b37aa075b7dd3399
+  # https://youtu.be/La3iWKRX-tE?t=1200
+  # https://medium.com/@noobhax/my-recon-process-dns-enumeration-d0e288f81a8a
+  while read line; do
+    IP=$(echo $line | awk '{ print $4 }')
+    PORT=$(echo $line | awk '{ print $3 }')
+    echo "[nmap] scanning IP:$IP using $PORT port"
+    FILENAME=$(echo $line | awk '{ print "nmap_"$4}' )
+    nmap -vv -sV --version-intensity 5 -sT -O --max-rate 5000 -Pn -T3 -p$PORT -oA ./$1/$foldername/nmap/$FILENAME $IP
+  done < ./$1/$foldername/masscan_output_tmp.txt
 }
 
 checkhttprobe(){
-  echo "[phase 2] Starting httpx probe testing..."
+  echo "[httpx] Starting httpx probe testing..."
   # resolve IP and hosts with http|https for bruteforce
-  httpx -l ./$1/$foldername/dnsprobe_ip.txt -silent -follow-host-redirects -fc 300,301,302,303 -o ./$1/$foldername/httpx_output_1.txt
-  httpx -l ./$1/$foldername/2-all-subdomains.txt -silent -follow-host-redirects -fc 300,301,302,303 -o ./$1/$foldername/httpx_output_2.txt
+  httpx -l ./$1/$foldername/dnsprobe_ip.txt -silent -follow-host-redirects -fc 300,301,302,303 -threads 300 -o ./$1/$foldername/httpx_output_1.txt
+  # httpx -l ./$1/$foldername/dnsprobe_subdomains.txt -silent -follow-host-redirects -fc 300,301,302,303 -threads 300 -o ./$1/$foldername/httpx_output_2.txt
+  touch ./$1/$foldername/httpx_output_2.txt
 
   sort -u ./$1/$foldername/httpx_output_1.txt ./$1/$foldername/httpx_output_2.txt > ./$1/$foldername/3-all-subdomain-live-scheme.txt
-  # tr -d '\[\]' < ./$1/$foldername/httpx_output.txt > ./$1/$foldername/tr_httpx_output.txt
-
-  # check file wirteup successfully from previous step
-  # while [ ! -s ./$1/$foldername/httpx_output.txt ]; do
-  #   echo "[checkhttprobe] httpx_output.txt empty, sleep 1"
-  #   sleep 1
-  # done
-
-  # split resolved hosts ans its IP (for masscan)
-  # cut -f1 -d ' ' ./$1/$foldername/httpx_output.txt > ./$1/$foldername/3-all-subdomain-live-scheme.txt
-  # cut -f2 -d ' ' ./$1/$foldername/httpx_output.txt | sort -u > ./$1/$foldername/3-all-subdomain-live-ip.txt
 }
 
 nucleitest(){
@@ -103,21 +142,6 @@ nucleitest(){
   fi
   echo "[phase 3] nuclei testing..."
   nuclei -l ./$1/$foldername/3-all-subdomain-live-scheme.txt -t ../nuclei-templates/generic-detections/ -t ../nuclei-templates/vulnerabilities/ -t ../nuclei-templates/security-misconfiguration/ -t ../nuclei-templates/cves/ -t ../nuclei-templates/misc/ -t ../nuclei-templates/files/ -t ../nuclei-templates/subdomain-takeover -o ./$1/$foldername/nuclei_output.txt
-}
-
-# nmap(){
-#   echo "[phase 7] Test for unexpected open ports..."
-#   nmap -sS -PN -T4 --script='http-title' -oG nmap_output_og.txt  
-# }
-masscantest(){
-  # max-rate for accuracy
-  masscan -p1-65535 -iL ./$1/$foldername/dnsprobe_ip.txt --max-rate 300 --open-only -oL ./$1/$foldername/masscan_output.txt
-}
-nmap_nse(){
-  awk '{ print $4 }' ./$1/$foldername/masscan_output.txt | sort | uniq > ./$1/$foldername/nmap_input.txt
-  nmap --script "auth" -iL ./$1/$foldername/nmap_input.txt
-  nmap --script=nfs-ls -iL ./$1/$foldername/nmap_input.txt
-
 }
 
 smuggler(){
@@ -135,7 +159,7 @@ smuggler(){
       echo "There are no Request Smuggling host found"
     fi
   else
-    echo "smuggler doesn't provide the output, check it issue!"
+    echo "smuggler doesn\'t provide the output, check it issue!"
   fi
 }
 
@@ -145,8 +169,8 @@ checkparams(){
     echo "Prepare custom wordlist using unfurl"
     cat ./$1/$foldername/wayback_output.txt | unfurl paths | sed 's/\///' > ./$1/$foldername/wayback_params_list.txt
     # merge base dirsearchWordlist with target-specific list for deep dive (time sensitive)
-    sort -u ./$1/$foldername/wayback_params_list.txt $dirsearchWordlist -o $dirsearchWordlist
-    sudo sed -i .bak '/^[[:space:]]*$/d' $dirsearchWordlist
+    sort -u ./$1/$foldername/wayback_params_list.txt $dirsearchWordlist -o $customFfufWordList
+    sudo sed -i .bak '/^[[:space:]]*$/d' $customFfufWordList
   fi
 }
 
@@ -155,7 +179,7 @@ ffufbrute(){
     echo "Start directory bruteforce using ffuf..."
     iterator=1
     while read subdomain; do
-      ffuf -c -u ${subdomain}/FUZZ -mc all -fc 300,301,302,303,304,500,501,502,503 -recursion -recursion-depth 3 -w $dirsearchWordlist -t $dirsearchThreads -o ./$1/$foldername/ffuf/${iterator}.csv -of csv
+      ffuf -c -u ${subdomain}/FUZZ -mc all -fc 300,301,302,303,304,500,501,502,503 -recursion -recursion-depth 3 -w $customFfufWordList -t $dirsearchThreads -o ./$1/$foldername/ffuf/${iterator}.csv -of csv
       iterator=$((iterator+1))
     done < ./$1/$foldername/3-all-subdomain-live-scheme.txt
   fi
@@ -168,12 +192,11 @@ recon(){
   permutatesubdomains $1
 
   dnsprobing $1
-  checkhttprobe $1
-
-  nucleitest $1
   masscantest $1
   nmap_nse $1
 
+  checkhttprobe $1
+  nucleitest $1
   smuggler $1
 
   checkparams $1
@@ -196,8 +219,22 @@ main(){
   fi
 
   mkdir ./$1/$foldername
-  # ffuf dir uses to store brute output
-  mkdir ./$1/$foldername/ffuf/
+
+  # used for ffuf bruteforce
+  touch ./$1/$foldername/custom_ffuf_wordlist.txt
+  customFfufWordList=./$1/$foldername/custom_ffuf_wordlist.txt
+  cp $dirsearchWordlist $customFfufWordList
+  # used to save target specific list for alterations (shuffledns, altdns)
+  touch ./$1/$foldername/custom_subdomains_wordlist.txt
+  customSubdomainsWordList=./$1/$foldername/custom_subdomains_wordlist.txt
+  cp $altdnsWordlist $customSubdomainsWordList
+
+  if [ "$brute" = "1" ]; then
+    # ffuf dir uses to store brute output
+    mkdir ./$1/$foldername/ffuf/
+  fi
+  # nmap output
+  mkdir ./$1/$foldername/nmap/
   # subfinder list of subdomains
   touch ./$1/$foldername/subfinder-list.txt 
   # assetfinder list of subdomains
@@ -220,7 +257,7 @@ main(){
 
 usage(){
   echo "Usage: $FUNCNAME <target> [[-b] | [--brute]] [[-m] | [--mad]]"
-  echo "Example: $FUNCNAME example.com -brute"
+  echo "Example: $FUNCNAME example.com --mad"
 }
 
 invokation(){
@@ -250,6 +287,10 @@ checkargs(){
                                   ;;
           -m | --mad )            mad="1"
                                   ;;
+          -a | --alt )            alt="1"
+                                  ;;
+          -w | --wildcard )       wildcard="1"
+                                  ;;
           # * )                     invokation $1
           #                         exit 1
       esac
@@ -274,11 +315,14 @@ if [ $# -gt 1 ]; then
   checkargs "$@"
 fi
 
+# positional parameters test
 echo "Check params: $@"
 echo "Check # of params: $#"
 echo "Check params \$1: $1"
 echo "Check params \$brute: $brute"
 echo "Check params \$mad: $mad"
+echo "Check params \$alt: $alt"
+echo "Check params \$wildcard: $wildcard"
 
 ./logo.sh
 path=$(pwd)
