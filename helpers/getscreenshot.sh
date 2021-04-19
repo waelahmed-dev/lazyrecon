84fd33312b91c8eb1358f2b73d225b6593ccfc71
storenth <storenth@yandex.ru>
2021-04-19 16:37:32 +0700

    update screenshot width and extend top-level domain scope

--- helpers/getscreenshot.sh
+++ helpers/getscreenshot.sh
@@ -4,5 +4,5 @@
 # https://developers.google.com/web/updates/2017/04/headless-chrome#screenshots
 
 # SCOPE=$(echo $1 | awk -F '//' '{print $NF}')
-SCOPE=$(echo $1 | grep -oriahE "(([[:alpha:][:digit:]-]+\.)+)?[[:alpha:][:digit:]-]+\.[[:alpha:]]{2,3}([:][[:digit:]]{2,4})?" | sed "s/:/_/;s/[.]/_/g")
-chromium --headless --disable-gpu --screenshot="${SCOPE}.png" $1
+SCOPE=$(echo $1 | grep -oriahE "(([[:alpha:][:digit:]-]+\.)+)?[[:alpha:][:digit:]-]+\.[[:alpha:]]{2,5}([:][[:digit:]]{2,4})?" | sed "s/:/_/;s/[.]/_/g")
+chromium --headless --disable-gpu --window-size=1280,720 --screenshot="${SCOPE}.png" $1
