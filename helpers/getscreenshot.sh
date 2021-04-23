8bf3d1215c1083fd8ec78ebb1880a975b85456a5
storenth <storenth@yandex.ru>
2021-04-23 20:39:46 +0700

    add full path parameter to chromium

--- helpers/getscreenshot.sh
+++ helpers/getscreenshot.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
+
+# https://bugs.chromium.org/p/chromium/issues/detail?id=1097565&can=2&q=component%3AInternals%3EHeadless
 
 # get page and take screenshot
 # https://developers.google.com/web/updates/2017/04/headless-chrome#screenshots
 
-# SCOPE=$(echo $1 | awk -F '//' '{print $NF}')
-SCOPE=$(echo $1 | grep -oriahE "(([[:alpha:][:digit:]-]+\.)+)?[[:alpha:][:digit:]-]+\.[[:alpha:]]{2,5}([:][[:digit:]]{2,4})?" | sed "s/:/_/;s/[.]/_/g")
-chromium --headless --disable-gpu --window-size=1280,720 --screenshot="${SCOPE}.png" $1
+# SCOPE=$(echo $2 | awk -F '//' '{print $NF}')
+SCOPE=$(echo $2 | grep -oriahE "(([[:alpha:][:digit:]-]+\.)+)?[[:alpha:][:digit:]-]+\.[[:alpha:]]{2,5}([:][[:digit:]]{2,4})?" | sed "s/:/_/;s/[.]/_/g")
+chromium --headless --disable-gpu --window-size=1280,720 --screenshot="${1}/${SCOPE}.png" $2
