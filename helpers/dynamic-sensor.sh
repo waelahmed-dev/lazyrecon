befa829bfdfcef9217ff729f70be9db4fc917f84
storenth <storenth@yandex.ru>
2021-11-13 17:16:28 +0700

    Update sensor to avoid hardcode

--- helpers/dynamic-sensor.sh
+++ helpers/dynamic-sensor.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 ITERATOR=0
-echo "starting up..."
 BAR='##############################'
 FILL='------------------------------'
-totalLines=$(wc -l ./live-targets-tmp.txt | awk '{print $1}')  # num. lines in file
+totalLines=$(wc -l "$2" | awk '{print $1}')  # num. lines in file
 barLen=30
-
-# --- iterate over lines in csv file ---
 count=0
-while read line; do
-    # update progress bar
-    count=$(($count + 1))
-    percent=$((($count * 100 / $totalLines * 100) / 100))
-    i=$(($percent * $barLen / 100))
-    echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] $count/$totalLines ($percent%)"
-    sleep 0.1
-done < ./live-targets-tmp.txt
+
+# --- iterate over lines in file ---
+sensor(){
+    while read line; do
+        # update progress bar
+        count=$(($count + 1))
+        percent=$((($count * 100 / $totalLines * 100) / 100))
+        i=$(($percent * $barLen / 100))
+        echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] $count/$totalLines ($percent%)"
+        eval "$1" > 
+    done < "$2"
+}
+
+sensor "$@"
