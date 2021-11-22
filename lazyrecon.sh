276db1c243f4a4a0795594c7430c6d55f7bb4946
storenth <storenth@yandex.ru>
2021-11-22 18:27:18 +0700

    Add `CUSTOMHEADER` to `gospider`

--- lazyrecon.sh
+++ lazyrecon.sh
@@ -331,7 +331,7 @@ gospidertest(){
   if [ -s $TARGETDIR/3-all-subdomain-live-scheme.txt ]; then
     echo
     echo "[$(date | awk '{ print $4}')] [gospider] Web crawling..."
-    gospider -q -r -S $TARGETDIR/3-all-subdomain-live-scheme.txt -o $TARGETDIR/gospider -c 40 -t 40 1> /dev/null
+    gospider -q -r -H "$CUSTOMHEADER" -S $TARGETDIR/3-all-subdomain-live-scheme.txt -o $TARGETDIR/gospider -c 40 -t 40 1> /dev/null
 
     # combine the results and filter out of scope
     cat $TARGETDIR/gospider/* > $TARGETDIR/tmp/gospider_raw_out.txt
